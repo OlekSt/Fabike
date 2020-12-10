9186b75c81f34a500468989a2952336173323f62
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-12-10 10:52:58 +0000

    Added home app & templates

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    """
+    A view to return landing page
+    """
+    return render(request, 'home/index.html')
