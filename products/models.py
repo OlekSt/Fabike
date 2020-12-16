360dff32006b8f2c84476a8d285daf43482f01b2
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-12-16 12:01:43 +0000

    Added Product models file & installed Pillow, updated requirements.txt

---
+++ products/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+# Create your models here.
+class Product(models.Model):
+
+    BIKES = 'BIKES'
+    FRAMES = 'FRAMES' 
+    PRODUCT_TYPE = [
+        (BIKES, 'BIKES'),
+        (FRAMES, 'FRAMES'),
+    ]
+
+    URBAN = 'URBAN'
+    ALL_ROAD = 'ALL ROAD'
+    ROAD = 'ROAD'
+    PRODUCT_GROUP = [
+        (URBAN, 'URBAN'),
+        (ALL_ROAD, 'ALL ROAD'),
+        (ROAD, 'ROAD'),
+    ]
+
+    product_type = models.CharField(
+        max_length=20,
+        choices=PRODUCT_TYPE,
+        default=BIKES,
+    )
+    product_group = models.CharField(
+        max_length=20,
+        choices=PRODUCT_GROUP,
+        default=URBAN,
+    )
+    name = models.CharField(max_length=80)
+    frame =	models.CharField(max_length=80)
+    title =	models.TextField(max_length=120)
+    fork =	models.CharField(max_length=80)
+    wheels = models.CharField(max_length=80, null=True, blank=True)
+    tyres = models.CharField(max_length=80, null=True, blank=True)
+    max_tyre_size = models.CharField(max_length=80, null=True, blank=True)
+    crankset = models.CharField(max_length=80, null=True, blank=True)
+    shift_levers = models.CharField(max_length=80, null=True, blank=True)
+    derailleurs	= models.CharField(max_length=80, null=True, blank=True)
+    casette_or_sprocket	= models.CharField(max_length=80, null=True, blank=True)
+    chain_or_belt = models.CharField(max_length=80, null=True, blank=True)
+    brakes = models.CharField(max_length=80, null=True, blank=True)
+    handlebar = models.CharField(max_length=80, null=True, blank=True)
+    stem = models.CharField(max_length=80, null=True, blank=True)
+    saddle = models.CharField(max_length=80, null=True, blank=True)
+    seatpost = models.CharField(max_length=80, null=True, blank=True)
+    seat_clamp = models.CharField(max_length=80, null=True, blank=True)
+    headset = models.CharField(max_length=80, null=True, blank=True)
+    seatpost_diameter = models.CharField(max_length=20, null=True, blank=True)
+    bottom_bracket = models.CharField(max_length=80, null=True, blank=True)
+    dropouts = models.CharField(max_length=120, null=True, blank=True)
+    weight = models.DecimalField(max_digits=4, decimal_places=2,
+                                 validators=[MinValueValidator(0.01)])
+    weight_alloy = models.DecimalField(max_digits=4, decimal_places=2,
+                                 validators=[MinValueValidator(0.01)])
+    weight_carbon = models.DecimalField(max_digits=4, decimal_places=2,
+                                 validators=[MinValueValidator(0.01)])
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                validators=[MinValueValidator(0.01)])
+    price_alloy = models.DecimalField(max_digits=6, decimal_places=2,
+                                validators=[MinValueValidator(0.01)])
+    price_carbon = models.DecimalField(max_digits=6, decimal_places=2,
+                                validators=[MinValueValidator(0.01)])
+    price_comment = models.CharField(max_length=120)
+    product_image01 = models.ImageField
+    product_image02 = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+            return self.name
