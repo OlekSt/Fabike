e623fdef7af33c403a48e5397f74603151da0f69
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-12-18 12:14:01 +0000

    created some space for pushing elements below navbar

--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 def bikes(request):
     """
     A view to return all bikes, showing 3 groups: urban, all-road, road 
     """
+    bikes = Product.objects.filter(product_type='BIKES')
+    context = {
+        'bikes': bikes,
+    }    
+    
     return render(request, 'products/bikes.html', context)
