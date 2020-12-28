ac35fd6426485354fa250e6d03178ed17875bcac
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-12-28 17:57:02 +0000

    Added template and view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def contact(request):
+    """
+    A view to return about page
+    """
+    return render(request, 'contact/contact.html')
