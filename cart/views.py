9fd1f9cbf1c151dce33434739c50604daa70598b
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-01-11 12:46:08 +0000

    Adding price carbon vs alloy in shopping cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def view_cart(request):
@@ -8,18 +10,48 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity'))
+    
     redirect_url = request.POST.get('redirect_url')
+    components = request.POST.get('components')
+    product = get_object_or_404(Product, pk=item_id)
+    if components == 'carbon':
+        price = product.price_carbon
+    else: 
+        price = product.price_alloy
+    #if 'components' in request.POST:
+    #    components = request.POST['components']
+    #price = request.POST['price_alloy']
+    #if 'price' in request.POST:
+    #    price = request.POST.get('price')
+    #elif 'price_carbon' in request.POST:
+    #    price = request.POST.get('price_carbon')
+    
     size = request.POST.get('size')
     color = request.POST.get('color')
-    components = request.POST.get('components')
-    cart = request.session.get('cart', {})
-   
-    cart[item_id] = quantity
+
+    #cart = request.session.get('cart', {})
+    
+    #if components:
+    #    if item_id in list(cart.keys()):
+    #        if components in cart[item_id]['items_by_size'].keys():
+    #            cart[item_id]['components_type'][size] += quantity
+    #        else:
+    #            cart[item_id]['components_type'][size] = quantity
+    #    else:
+    #        cart[item_id] = {'components_type': {components: quantity}}
+    #else:
+    #    if item_id in list(cart.keys()):
+    #        cart[item_id] += quantity
+    #    else:
+    #        cart[item_id] = quantity
         
     
-    request.session['cart'] = cart
-    print(request.session['cart'])
+    #request.session['cart'] = cart
+    #print(request.session['cart'])
+    print(components)
+    print(size)
+    print(color)
+    print(price)
     
     
     return redirect(redirect_url)
--- products/static/products/js/product.js
+++ products/static/products/js/product.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     $("input[type=radio][name=components]").on('change', function() {
         var componentChoice = $("input[type=radio][name=components]:checked").val();
         if (componentChoice == "alloy") {
-            $("#price-alloy, #weight-alloy").removeClass("price-muted").addClass("text-white").change(); 
-            $("#price-carbon, #weight-carbon").removeClass("text-white").addClass("price-muted").change();
+            $("#price_alloy, #weight_alloy").removeClass("price-muted").addClass("text-white").change(); 
+            $("#price_carbon, #weight_carbon").removeClass("text-white").addClass("price-muted").change();
         }
         else if (componentChoice == "carbon") {
-            $("#price-carbon, #weight-carbon").removeClass("price-muted").addClass("text-white").change(); 
-            $("#price-alloy, #weight-alloy").removeClass("text-white").addClass("price-muted").change();
+            $("#price_carbon, #weight_carbon").removeClass("price-muted").addClass("text-white").change(); 
+            $("#price_alloy, #weight_alloy").removeClass("text-white").addClass("price-muted").change();
         }
     });
 
