27d30de522c563c2b89b7ee6b006abf4d7ebf5aa
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-01-12 08:13:20 +0000

    Added functionality - Quantity is added to shopping cart by components type

--- cart/contexts.py
+++ cart/contexts.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from . import views
 
 
 def cart_contents(request):
@@ -9,24 +10,25 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+        
+    
     cart = request.session.get('cart', {})
-    #if 'price' in request.POST:
-    #    price = request.POST['price']
-    #elif 'price_carbon' in request.POST:
-    #    price = request.POST['price_carbon']
-    #else:
-    #    price = request.POST['price_alloy']
-    """
+
+   
+
+    
+
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-    #    total =+ quantity * product.price
-        product_count =+ quantity
+        
+        
+        
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
         })
-    """
+    
     context = {
         'cart_items': cart_items, 
         'total': total,
