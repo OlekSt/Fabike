f585c68501ee5016000443128c8f38318fbef700
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-01-25 09:51:22 +0000

    Added profiles template, urls, views

--- fabike/urls.py
+++ fabike/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
     path('cart/', include('cart.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('profile/', include('profiles.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
