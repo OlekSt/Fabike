edc03e5cfb005c4d03fbf389017b0b3f4b40b69d
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-02 13:10:22 +0000

    Added checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, \
+    reverse, get_object_or_404, HttpResponse
 
-# Create your views here.
+from django.conf import settings
+from django.contrib import messages
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, 'There is nothing \
+                                 in your cart now!')
+        return redirect(reverse('bikes'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- fabike/urls.py
+++ fabike/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
