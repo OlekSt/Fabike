c2fc398fc002ac2238a0993c0cab7bbae4d5e635
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-06 14:28:15 +0000

    User sign up/in/out works

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Profile
 
 
 def profile(request):
     """ Display the user's profile. """
-
-    context = {}
+    profile = get_object_or_404(Profile, user=request.user)
+    context = {
+        'profile': profile,
+    }
 
     return render(request, 'profiles/profile.html', context)
