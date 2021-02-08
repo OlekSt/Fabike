fc17e0fb8c8b42a22782b600d2e3c583155819dc
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-08 16:07:57 +0000

    Added custom widget for file loading in product management

--- fabike/settings.py
+++ fabike/settings.py
@@ -168,6 +168,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
