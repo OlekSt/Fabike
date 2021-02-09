3f8d87a66355ef9d4ab506c73dd437da39866404
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-09 08:23:04 +0000

    Deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn fabike.wsgi:application
--- fabike/settings.py
+++ fabike/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'exmc++7b_35-xudnmbce-+w=r7t@yq^nnc0bjr2i6g1_c6o0te'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fabike.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'fabike.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
