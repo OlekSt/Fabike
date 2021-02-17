e7d0a301df2c6bad74311a331f7fc482e20430cf
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-17 07:13:33 +0000

    Moved hero image slide show javascript from base.js to home.js

---
+++ home/static/home/js/home.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+// hero image slideshow
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1};    
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active-slide", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active-slide";
+        setTimeout(showSlides, 4000); // Change image every 4 seconds
+            
+    }
+});
