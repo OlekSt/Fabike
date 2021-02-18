a0493c99638c0242a1bbe79a557fdef6a0e85d2c
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-18 12:57:17 +0000

    Updated field Is Bike = Yes/No to be dynamically changed

--- products/static/products/js/product_admin.js
+++ products/static/products/js/product_admin.js
@@ -1,25 +1,29 @@
 $(document).ready(function () {
+    //Sets fields ONLOAD
     let productTypeChoice = $("#id_product_type").val();
     if (productTypeChoice == "FRAMES") {
-        // Onload to hide fields which are only for bikes
+        // to hide fields which are only for bikes
         $("#div_id_wheels, #div_id_tyres, #div_id_crankset, #div_id_shift_levers, #div_id_derailleurs, #div_id_casette_or_sprocket, #div_id_chain_or_belt, #div_id_handlebar, #div_id_stem, #div_id_saddle, #div_id_seatpost, #div_id_price_alloy, #div_id_price_carbon, #div_id_weight_carbon, #div_id_weight_alloy").hide();
+        $("#id_is_bike").val("false").change();
     }
-    // Onload to hide fields which are only for frames
+    // to hide fields which are only for frames
     else if (productTypeChoice == "BIKES") {
         $("#div_id_headset, #div_id_seat_clamp, #div_id_bottom_bracket, #div_id_seatpost_diameter, #div_id_dropouts, #div_id_max_tyre_size, #div_id_price, #div_id_weight").hide(); 
     }
-    //Updated fields visibility based on users' choice
+    //Updates fields visibility based on users' choice
     $("#id_product_type").change( function() {
         var productTypeChoice = $("#id_product_type").val();
         if (productTypeChoice == "BIKES") {
             // On user's choice to show fields which are only for bikes
             $("#div_id_wheels, #div_id_tyres, #div_id_crankset, #div_id_shift_levers, #div_id_derailleurs, #div_id_casette_or_sprocket, #div_id_chain_or_belt, #div_id_handlebar, #div_id_stem, #div_id_saddle, #div_id_seatpost, #div_id_price_alloy, #div_id_price_carbon, #div_id_weight_carbon, #div_id_weight_alloy").show(); 
+            $("#id_is_bike").val("true").change();
             // On user's choice to hide fields for frames
             $("#div_id_headset, #div_id_seat_clamp, #div_id_bottom_bracket, #div_id_seatpost_diameter, #div_id_dropouts, #div_id_max_tyre_size, #div_id_price, #div_id_weight").hide(); 
         }
         else if (productTypeChoice == "FRAMES") {
             // On user's choice to hide fields which are only for bikes
             $("#div_id_wheels, #div_id_tyres, #div_id_crankset, #div_id_shift_levers, #div_id_derailleurs, #div_id_casette_or_sprocket, #div_id_chain_or_belt, #div_id_handlebar, #div_id_stem, #div_id_saddle, #div_id_seatpost, #div_id_price_alloy, #div_id_price_carbon, #div_id_weight_carbon, #div_id_weight_alloy").hide(); 
+            $("#id_is_bike").val("false").change();
             // On user's choice to show fields for frames
             $("#div_id_headset, #div_id_seat_clamp, #div_id_bottom_bracket, #div_id_seatpost_diameter, #div_id_dropouts, #div_id_max_tyre_size, #div_id_price, #div_id_weight").show(); 
         }
