8d136ef78462f4c98fafe00c73f794ab44861e2f
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2021-02-22 15:57:19 +0000

    Fixed pre-filling of order form if profile data extists

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -42,7 +42,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='OrderLineItem',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
                 ('product_color', models.CharField(blank=True, max_length=10, null=True)),
                 ('product_size', models.CharField(blank=True, max_length=2, null=True)),
                 ('product_components', models.CharField(blank=True, max_length=15, null=True)),
--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Event
-import datetime
 
 
 class EventForm(forms.ModelForm):
