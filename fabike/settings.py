ae3565a459c5b8656cbc598c7d9f3b723f78d8b5
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2024-02-02 10:43:41 +0000

    Updated ALLOWED_HOSTS

--- fabike/settings.py
+++ fabike/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['fabike.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['fabike.onrender.com', 'localhost']
 
 
 # Application definition
